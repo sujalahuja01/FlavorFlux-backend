e4b50638218ef3c390be7ff17057af4c3e11f1be
sujalahuja01 <sujalahuja150@gmail.com>
2025-09-18 19:17:41 +0530

    Updated config.py file

--- chef/app/config.py
+++ chef/app/config.py
@@ -16,3 +16,6 @@ class Config:
     MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
     MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
     MAIL_DEFAULT_SENDER = os.environ.get("MAIL_DEFAULT_SENDER")
+
+    SESSION_COOKIE_SAMESITE = "None"
+    SESSION_COOKIE_SECURE = False
