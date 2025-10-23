630ff4f67fd92217e03d07fd630c6872b2a292e2
sujalahuja01 <sujalahuja150@gmail.com>
2025-10-23 20:16:30 +0530

    updated run.py

--- chef/run.py
+++ chef/run.py
@@ -1,29 +1,11 @@
-# import os
-# from dotenv import load_dotenv
-# from chef.app.app import create_app
-#
-# dotenv_path = os.path.join(os.path.dirname(__file__), ".env")
-# load_dotenv(dotenv_path)
-#
-# chef_app = create_app()
-#
-# if __name__ == "__main__":
-#     chef_app.run()
-#
-#
 import os
 from dotenv import load_dotenv
 from chef.app.app import create_app
-from flask_migrate import upgrade
 
 dotenv_path = os.path.join(os.path.dirname(__file__), ".env")
 load_dotenv(dotenv_path)
 
 chef_app = create_app()
-
-# Apply migrations automatically (one-time)
-with chef_app.app_context():
-    upgrade()
 
 if __name__ == "__main__":
     chef_app.run()
